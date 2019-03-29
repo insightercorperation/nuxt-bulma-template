7a25af28cc118c2378a00ca6d47f549058052b12
sungeunDev <starfg62@gmail.com>
2019-03-29 17:56:16 +0900

    Add TeamMemberSection, TeamMemberCard Component with samples

---
+++ components/TeamMemberSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="hero-is-medium">
+    <div class="columns is-multiline is-variable is-8">
+      <teamMemberCard
+        v-for="member in members"
+        :key="member.name"
+        :name="member.name"
+        :job="member.job"
+        :desc="member.desc"
+        :image="member.image"
+        :hover-image="member.hoverImage"
+        class="column is-4 ins-card-element"
+      >
+      </teamMemberCard>
+    </div>
+  </section>
+</template>
+
+<script>
+import TeamMemberCard from '~/components/TeamMemberCard'
+
+export default {
+  components: {
+    TeamMemberCard
+  },
+  props: {
+    members: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.ins-card-element {
+  margin: 0 2rem 2rem 2rem;
+}
+
+@media screen and (min-width: 769px), print {
+  .ins-card-element {
+    margin: 0 0rem 5rem 0rem;
+  }
+}
+</style>
