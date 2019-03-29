09f53508f01c985fb65c9681e2be624889c4b456
tae hoon kang <kangthink@gmail.com>
2019-03-29 11:47:00 +0900

    Fix column margin in DescSection and delete unused unittests

--- test/specs/SponsorSection.spec.js
+++ test/specs/SponsorSection.spec.js
@@ -4,23 +4,5 @@ import SponsorSection from '../../components/SponsorSection.vue'
 
 test('is a Vue instance', t => {
   const wrapper = mount(SponsorSection)
-  console.log(wrapper)
   t.is(wrapper.isVueInstance(), true)
 })
-
-// test('List.vue shapshow', t => {
-//   let images = ['img/logo/buefy.png']
-//   const wrapper = mount(SponsorSection, {
-//     propsData: { images }
-//   })
-//   console.log(wrapper.html())
-//   t.snapshot({ html: wrapper.html() })
-// })
-
-// test('List.vue ddd', t => {
-//   // let images = ['img/logo/buefy.png']
-//   const wrapper = mount(SponsorSection)
-//   const container = wrapper.find('.section')
-//   console.log(container.value)
-//   t.snapshot({ html: wrapper.html() })
-// })
